8a90bfa8babd016924e55f4422c0d0247e8510c1
heet003 <heet.b003@gmail.com>
2024-05-26 18:06:29 +0530

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Card/Card.css 	new file:   src/components/Card/Card.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>
+          Crypto News <span>App</span>
+        </h1>
+        <p>Stay updated with cryptocurrency</p>
+      </div>
+      <div className="news-container">
+        <Card>
+          <img
+            src="https://i-invdn-com.investing.com/news/LYNXMPEE0H0B5_L.jpg"
+            alt=""
+          ></img>
+          <h3>
+            SEC Carefully Considering Spot Bitcoin ETF in Response to
+            Congressman
+          </h3>
+        </Card>
+        <Card>
+          <img
+            src="https://i-invdn-com.investing.com/news/moved_LYNXMPEI0O0MB-ORUBS_L.jpg"
+            alt=""
+          ></img>
+          <h3>Fidelity Exec Compares Bitcoin to Apple</h3>
+        </Card>
+        <Card>
+          <img
+            src="https://i-invdn-com.investing.com/news/LYNXMPED990HX_L.jpg"
+            alt=""
+          ></img>
+          <h3>
+            Yen bid, bitcoin battered as Ukraine fears leave traders nervous
+          </h3>
+        </Card>
+        <Card>
+          <img
+            src="https://i-invdn-com.investing.com/news/moved_LYNXMPEI0O0MB-ORUBS_L.jpg"
+            alt=""
+          ></img>
+          <h3>Fidelity Exec Compares Bitcoin to Apple</h3>
+        </Card>
+      </div>
     </div>
   );
 }
