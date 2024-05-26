f7a76e8e1213b0685ec33bae943b54a80a7e0d31
heet003 <heet.b003@gmail.com>
2024-05-26 19:04:00 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Card/Card.css

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Card from "./components/Card/Card";
+import axios from "axios";
 
 function App() {
+  const [news, setNews] = useState([]);
+  const options = {
+    method: "GET",
+    url: "https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-news?pair_ID=1057391&page=1&time_utc_offset&lang_ID=1",
+    headers: {
+      "X-RapidAPI-Key": "dfeee1f073msh5f9bc151904928cp1f3eddjsn35fd98560012",
+      "X-RapidAPI-Host": "investing-cryptocurrency-markets.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.request(options);
+        const data = response.data.data[0].screen_data.news;
+        setNews(data);
+        console.log(response.data.data[0].screen_data.news);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <div className="header">
         <h1>
           Crypto News <span>App</span>
         </h1>
-        <p>Stay updated with cryptocurrency</p>
+        <p>Stay Updated With Market Of Cryptocurrency</p>
       </div>
       <div className="news-container">
-        <Card>
-          <img
-            src="https://i-invdn-com.investing.com/news/LYNXMPEE0H0B5_L.jpg"
-            alt=""
-          ></img>
-          <h3>
-            SEC Carefully Considering Spot Bitcoin ETF in Response to
-            Congressman
-          </h3>
-        </Card>
-        <Card>
-          <img
-            src="https://i-invdn-com.investing.com/news/moved_LYNXMPEI0O0MB-ORUBS_L.jpg"
-            alt=""
-          ></img>
-          <h3>Fidelity Exec Compares Bitcoin to Apple</h3>
-        </Card>
-        <Card>
-          <img
-            src="https://i-invdn-com.investing.com/news/LYNXMPED990HX_L.jpg"
-            alt=""
-          ></img>
-          <h3>
-            Yen bid, bitcoin battered as Ukraine fears leave traders nervous
-          </h3>
-        </Card>
-        <Card>
-          <img
-            src="https://i-invdn-com.investing.com/news/moved_LYNXMPEI0O0MB-ORUBS_L.jpg"
-            alt=""
-          ></img>
-          <h3>Fidelity Exec Compares Bitcoin to Apple</h3>
-        </Card>
+        {news.map((item, index) => (
+          <Card key={item.news_ID}>
+            <a href={item.news_link} target="_blank" rel="noopener noreferrer">
+              <img src={item.related_image_big} alt="" />
+              <h3>{item.HEADLINE}</h3>
+            </a>
+          </Card>
+        ))}
       </div>
     </div>
   );
